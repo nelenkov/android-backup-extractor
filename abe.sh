59782cdf81a6136880a15516125ee4368c4f0fd6
Nikolay Elenkov <nikolay.elenkov@gmail.com>
2014-06-09 23:49:45 +0900

    use BC 1.5

--- abe.sh
+++ abe.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-JAVA=/usr/local/jdk1.7.0_04/bin/java
-BC=./lib/bcprov-jdk15on-148.jar
+JAVA=/usr/local/jdk1.7.0_51/bin/java
+BC=./lib/bcprov-jdk15on-150.jar
 CP=:bin:$BC
 
 "$JAVA" -cp $CP org.nick.abe.Main $*
